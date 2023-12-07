07ccfb97cf5aa2fb82f4e4b64c89f34a85e00ff1
+ <+>
2023-12-07 11:52:11 +0300

    first commit

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import Table, ForeignKey, Column, Integer, String, MetaData
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -18,6 +18,8 @@ class Company(Base):
 
     def __repr__(self):
         return f'<Company {self.name}>'
+    freebie=relationship('Freebie',backref='company')
+    devs=relationship('Dev',secondary='company_dev',back_populates='companies')
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -27,3 +29,21 @@ class Dev(Base):
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+    freebie=relationship('Freebie',backref='dev')
+    companies=relationship('Company',secondary='company_dev', back_populates='devs')
+class Freebie(Base):
+    __tablename__='freebies'
+
+    id=Column(Integer(), primary_key=True)
+    item_name=Column(String())
+    value=Column(Integer())
+    company_id=Column(Integer(), ForeignKey('companies.id'))
+    dev_id=Column(Integer(), ForeignKey('devs.id'))
+
+company_dev=Table(
+    'companies_devs',
+    Base.metadata,
+    Column('company_id',ForeignKey('companies.id'), primary_key=True),
+    Column('dev_id',ForeignKey('devs.id'), primary_key=True),
+    extend_existing=True,
+)
--- lib/seed.py
+++ lib/seed.py
@@ -1,3 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+from models import Company,Dev,Freebie,company_dev
+from sqlalchemy.orm import Session
 
 # Script goes here!
+companies_info=[{
+  "id": 1,
+  "name": "Devcast",
+  "founding_year": 1993
+}, {
+  "id": 2,
+  "name": "Kwinu",
+  "founding_year": 2010
+}, {
+  "id": 3,
+  "name": "Gigashots",
+  "founding_year": 2008
+}, {
+  "id": 4,
+  "name": "Voonder",
+  "founding_year": 1984
+}, {
+  "id": 5,
+  "name": "Chatterpoint",
+  "founding_year": 1992
+}, {
+  "id": 6,
+  "name": "Eabox",
+  "founding_year": 1998
+}, {
+  "id": 7,
+  "name": "Zoovu",
+  "founding_year": 2005
+}, {
+  "id": 8,
+  "name": "Zava",
+  "founding_year": 1987
+}, {
+  "id": 9,
+  "name": "Vitz",
+  "founding_year": 2011
+}, {
+  "id": 10,
+  "name": "Kazu",
+  "founding_year": 2004
+}]
