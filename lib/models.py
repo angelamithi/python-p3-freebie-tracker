cc3b99c690099fb220b94657ee50765a426bfc85
+ <+>
2023-12-07 13:16:25 +0300

    second commit

--- lib/models.py
+++ lib/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Table, ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+# models.py
+from sqlalchemy import Table, ForeignKey, Column, Integer, String, MetaData, create_engine
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 convention = {
@@ -18,32 +19,39 @@ class Company(Base):
 
     def __repr__(self):
         return f'<Company {self.name}>'
-    freebie=relationship('Freebie',backref='company')
-    devs=relationship('Dev',secondary='company_dev',back_populates='companies')
+    freebie = relationship('Freebie', backref='company')
+    devs = relationship('Dev', secondary='company_dev', back_populates='companies')
 
 class Dev(Base):
     __tablename__ = 'devs'
 
     id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    name = Column(String())
 
     def __repr__(self):
         return f'<Dev {self.name}>'
-    freebie=relationship('Freebie',backref='dev')
-    companies=relationship('Company',secondary='company_dev', back_populates='devs')
+    freebie = relationship('Freebie', backref='dev')
+    companies = relationship('Company', secondary='company_dev', back_populates='devs')
+
 class Freebie(Base):
-    __tablename__='freebies'
+    __tablename__ = 'freebies'
 
-    id=Column(Integer(), primary_key=True)
-    item_name=Column(String())
-    value=Column(Integer())
-    company_id=Column(Integer(), ForeignKey('companies.id'))
-    dev_id=Column(Integer(), ForeignKey('devs.id'))
+    id = Column(Integer(), primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
+    dev_id = Column(Integer(), ForeignKey('devs.id'))
 
-company_dev=Table(
+company_dev = Table(
     'companies_devs',
     Base.metadata,
-    Column('company_id',ForeignKey('companies.id'), primary_key=True),
-    Column('dev_id',ForeignKey('devs.id'), primary_key=True),
+    Column('company_id', ForeignKey('companies.id'), primary_key=True),
+    Column('dev_id', ForeignKey('devs.id'), primary_key=True),
     extend_existing=True,
 )
+
+engine = create_engine('sqlite:///freebies.db')
+Base.metadata.create_all(engine)  
+
+Session = sessionmaker(bind=engine)
+session = Session()
--- lib/seed.py
+++ lib/seed.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-from models import Company,Dev,Freebie,company_dev
-from sqlalchemy.orm import Session
+#seed.py
+from models import Company,Dev,Freebie,company_dev,session
 
 # Script goes here!
+session.query(Company).delete()
+session.query(Dev).delete()
+session.query(Freebie).delete()
+session.commit()
 companies_info=[{
   "id": 1,
   "name": "Devcast",
@@ -44,3 +48,119 @@ companies_info=[{
   "name": "Kazu",
   "founding_year": 2004
 }]
+
+session.add_all([Company(**company) for company in companies_info])
+session.commit()
+
+devs_info=[{
+  "id": 1,
+  "name": "Ingaborg Maher"
+}, {
+  "id": 2,
+  "name": "Elijah Shiel"
+}, {
+  "id": 3,
+  "name": "Isidro Axford"
+}, {
+  "id": 4,
+  "name": "Lou Volleth"
+}, {
+  "id": 5,
+  "name": "Regan Jermin"
+}, {
+  "id": 6,
+  "name": "Kristel Fawdery"
+}, {
+  "id": 7,
+  "name": "Sonnnie Rubinowicz"
+}, {
+  "id": 8,
+  "name": "Bobette Beaufoy"
+}, {
+  "id": 9,
+  "name": "Maximo Elderton"
+}, {
+  "id": 10,
+  "name": "Laurianne Kilgrove"
+}]
+
+session.add_all([Dev(**dev) for dev in devs_info])
+session.commit()
+
+freebie_info=[
+  {
+    "id": 1,
+    "item_name": "Laptop",
+    "value": 1500,
+    "company_id": 3,
+    "dev_id": 8
+  },
+  {
+    "id": 2,
+    "item_name": "Mouse",
+    "value": 20,
+    "company_id": 5,
+    "dev_id": 2
+  },
+  {
+    "id": 3,
+    "item_name": "Keyboard",
+    "value": 50,
+    "company_id": 7,
+    "dev_id": 6
+  },
+  {
+    "id": 4,
+    "item_name": "USB Drive",
+    "value": 10,
+    "company_id": 1,
+    "dev_id": 9
+  },
+  {
+    "id": 5,
+    "item_name": "Headphones",
+    "value": 30,
+    "company_id": 9,
+    "dev_id": 4
+  },
+  {
+    "id": 6,
+    "item_name": "Webcam",
+    "value": 40,
+    "company_id": 2,
+    "dev_id": 7
+  },
+  {
+    "id": 7,
+    "item_name": "External Hard Drive",
+    "value": 80,
+    "company_id": 8,
+    "dev_id": 1
+  },
+  {
+    "id": 8,
+    "item_name": "Monitor",
+    "value": 200,
+    "company_id": 4,
+    "dev_id": 10
+  },
+  {
+    "id": 9,
+    "item_name": "Wireless Mouse",
+    "value": 25,
+    "company_id": 6,
+    "dev_id": 5
+  },
+  {
+    "id": 10,
+    "item_name": "Graphics Card",
+    "value": 300,
+    "company_id": 10,
+    "dev_id": 3
+  }
+]
+session.add_all([Freebie(**freebie) for freebie in freebie_info])
+session.commit()
+
+
+
